31318151c69c446a0358d34671fc54273febfe27
macrosong <macrosonglx@126.com>
2019-02-25 17:17:52 +0800

    first commit

---
+++ src/test/java/HomeControllerTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import spittr.web.HomeController;
+
+
+public class HomeControllerTest {
+    @Test
+    public void testHome() throws Exception {
+        HomeController homeController = new HomeController();
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.view().name("home"));
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: marco
+  Date: 19-2-25
+  Time: 下午4:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Helloworld!</title>
+  </head>
+  <body>
+  Helloworld 这是一个测试。
+  </body>
+</html>
