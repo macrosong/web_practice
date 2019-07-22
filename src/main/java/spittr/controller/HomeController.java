ec1158529d3a02f3af07c39eecfe20479b28f724
macrosong <macrosonglx@126.com>
2019-07-23 00:04:54 +0800

    git bugfix

--- src/main/java/spittr/controller/HomeController.java
+++ src/main/java/spittr/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import spittr.cfg.GlobalConstants;
 import spittr.util.HashUtil;
@@ -29,9 +30,16 @@ public class HomeController {
 
     @RequestMapping(value = "/autoget", method = RequestMethod.GET)
     public String autoGet() {
-        String id = Long.toString(jedisPool.getResource().incr("auto_incr"));
-        String content = "test:content:" + id;
-        jedisPool.getResource().lpush(GlobalConstants.REDIS_SRC_DATA_KEY, id + "_" + content);
-        return "id: " + id + " content: " + content;
+        Jedis jedis = jedisPool.getResource();
+        try {
+            String id = Long.toString(jedis.incr("auto_incr"));
+            String content = "test:content:" + id;
+            jedis.lpush(GlobalConstants.REDIS_SRC_DATA_KEY, id + "_" + content);
+            return "id: " + id + " content: " + content;
+        } finally {
+            if(jedis != null) {
+                jedis.close();
+            }
+        }
     }
 }
